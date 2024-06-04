4f28415d91addf861c73e5efd031e6a3c93cfd1b
André Ganda <andre.ganda@pm.es.gov.br>
2024-06-04 00:55:39 -0300

    Inicio tela de vendas

---
+++ lojaHemilly/Controllers/VendasController.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using lojaHemilly.DataBase;
+using lojaHemilly.Models;
+
+namespace lojaHemilly.Controllers
+{
+    public class VendasController : Controller
+    {
+        private readonly FlorDeLizContext _context;
+
+        public VendasController(FlorDeLizContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Vendas
+        public async Task<IActionResult> Index()
+        {
+            var florDeLizContext = _context.Vendas.Include(v => v.Cliente);
+            return View(await florDeLizContext.ToListAsync());
+        }
+
+        // GET: Vendas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var venda = await _context.Vendas
+                .Include(v => v.Cliente)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (venda == null)
+            {
+                return NotFound();
+            }
+
+            return View(venda);
+        }
+
+        // GET: Vendas/Create
+        public IActionResult Create()
+        {
+
+            //select Item
+            ViewData["ClienteId"] = ListaClientesSelectItem();
+            return View();
+        }
+
+        private IEnumerable<SelectListItem> ListaClientesSelectItem()
+        {
+            var listaManual = new List<SelectListItem>
+            {
+                new SelectListItem { Text = "SELECIONE UM CLIENTE ", Value = "" }
+            };
+
+            // Criando a lista selecionável a partir dos clientes
+            var listaSelecionavelClientes = new SelectList(_context.Clientes, "ClienteID", "Nome");
+
+            // Concatenando a lista manual com a lista selecionável dos clientes
+            var listaCompleta = listaManual.Concat(listaSelecionavelClientes);
+
+            return listaCompleta;
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,DataDaVenda,ClienteId,PrecoTotal,NumeroParcelas,Total")] Venda venda)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(venda);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "ClienteID", "Nome", venda.ClienteId);
+            return View(venda);
+        }
+
+        // GET: Vendas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var venda = await _context.Vendas.FindAsync(id);
+            if (venda == null)
+            {
+                return NotFound();
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "ClienteID", "Nome", venda.ClienteId);
+            return View(venda);
+        }
+
+        // POST: Vendas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,DataDaVenda,ClienteId,PrecoTotal,NumeroParcelas,Total")] Venda venda)
+        {
+            if (id != venda.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(venda);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VendaExists(venda.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "ClienteID", "Nome", venda.ClienteId);
+            return View(venda);
+        }
+
+        // GET: Vendas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var venda = await _context.Vendas
+                .Include(v => v.Cliente)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (venda == null)
+            {
+                return NotFound();
+            }
+
+            return View(venda);
+        }
+
+        // POST: Vendas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var venda = await _context.Vendas.FindAsync(id);
+            if (venda != null)
+            {
+                _context.Vendas.Remove(venda);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VendaExists(int id)
+        {
+            return _context.Vendas.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ lojaHemilly/Views/Vendas/Js/Create.js
@@ -0,0 +1,3 @@
+﻿jQuery(document).ready(function () {
+    $("#ClienteId").select2();
+});
